45b373a983d76d7e4e0632545b26619bc7cf08a2
Unget343 <zaharkovtonuk36@gmail.com>
2026-03-22 09:29:39 +0000

    Добавить поддержку предупреждений компилятора и функцию для их настройки в CMake

---
+++ src/cmake/warnings.cmake
@@ -0,0 +1,31 @@
+function(target_set_warnings targget enable as_errors)
+    if (NOT ${ENABLED})
+        message(STATUS "Warnings disabled for: ${target}")
+        return()
+    endif()
+
+    set(MSVC_COMPILER /W4 /permissive-)
+
+    set(CLANG_COMPILER -Wall -Wextra -Wpedantic)
+
+    set(GCC_WARNINGS ${CLANG_WARNINGS})
+
+    if (${AS_ERRORS})
+        set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
+        set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
+        set(GCC_WARNINGS ${GCC_WARNINGS} -Werror)
+    endif()
+
+    set(CXX_COMPILER ${CMAKE_CXX_COMPILER_ID})
+
+    if(CXX_COMPILER MATCHES "MSVC")
+        set(WARNINGS ${MSVC_WARNINGS})
+    elseif(CXX_COMPILER MATCHES "clang")
+        set(WARNINGS ${CLANG_WARNINGS})
+    elseif(CXX_COMPILER MATCHES "GNU")
+        set(WARNINGS ${GCC_WARNINGS})
+    endif()
+
+    target_compiler_options(${TARGET} PRIVATE ${WARNINGS})
+    
+endfunction()
--- src/lib/my_lib/my_lib.cc
+++ src/lib/my_lib/my_lib.cc
@@ -3,8 +3,10 @@
 #include <cstdint>
 using namespace std;
 
+#ifdef PRINTER_ACTIVE
 void print_hello()
 { cout << "Hello, World!" << endl; }
+#endif
 
 std::uint32_t factorial(std::uint32_t number)
 { return number <= 1 ? number : factorial(number - 1) * number; }
